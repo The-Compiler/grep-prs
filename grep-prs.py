63497a05aea6daee124a650153aee0431c886d4f
Florian Bruhin <me@the-compiler.org>
2021-02-23 21:36:17 +0100

    Rewrite using github3.py

--- grep-prs.py
+++ grep-prs.py
@@ -1,56 +1,53 @@
 import re
 import sys
-import subprocess
+import pathlib
+
 import rich.progress
-
-def call_git(*args):
-    proc = subprocess.run(['git', *args], capture_output=True, encoding='utf-8', check=True)
-    return proc.stdout.splitlines()
-
-
-def _parse_data(lines):
-    prefix = 'refs/pull/'
-    suffix = '/head'
-    for line in lines:
-        commit, ref = line.split()
-        assert ref.startswith(prefix), ref
-        assert ref.endswith(suffix), ref
-        yield commit, int(ref[len(prefix):-len(suffix)])
+import rich.panel
+import rich.syntax
+import github3
 
 
 def _grep_diff(diff, pattern):
     for line in diff:
+        if not line.startswith(('+', '-')):
+            continue
         if pattern.search(line):
             yield line
 
 
-def _pr_header(console, pr):
-    console.rule(f'[blue]PR #{pr}[/blue]')
+def _print_pr_header(console, pr):
+    title = f'[magenta]PR #{pr.number}[/magenta]'
+    panel = rich.panel.Panel(
+            f"[blue]{pr.html_url}[/blue]\n"
+            f"[bright_black]{pr.title}[/bright_black]\n"
+            f"[yellow]@{pr.user.login}[/yellow]", expand=False, title=title)
+    console.print('')
+    console.print(panel)
 
 
 def run(progress):
     task = progress.add_task('Getting PRs...', start=False)
-    lines = call_git('ls-remote', 'origin', 'pull/*/head')
-    data = sorted(_parse_data(lines), key=lambda pair: pair[1])
+
+    token = pathlib.Path('~/.gh_token').expanduser().read_text().strip()
+    gh = github3.login(token=token)
+    repo = gh.repository('qutebrowser', 'qutebrowser')
+    prs = list(repo.pull_requests(state='open'))
 
     pattern = re.compile(sys.argv[1])
 
-    progress.update(task, description='Grepping PRs...', total=len(data))
+    progress.update(task, description='Grepping PRs...', total=len(prs))
     progress.start_task(task)
-    for commit, pr in data:
+    for pr in prs:
         progress.update(task, advance=1)
 
-        try:
-            diff = call_git('diff', '-U0', f'HEAD...{commit}')
-        except subprocess.CalledProcessError as e:
-            _pr_header(progress.console, pr)
-            progress.console.print(f'[red]{e.stderr.strip()}[/red]')
-            continue
-
+        diff = pr.patch().decode('utf-8').splitlines()
         matches = list(_grep_diff(diff, pattern))
+
         if matches:
-            _pr_header(progress.console, pr)
-            progress.console.print('\n'.join(matches))
+            _print_pr_header(progress.console, pr)
+            syntax = rich.syntax.Syntax('\n'.join(matches), 'diff')
+            progress.console.print(syntax)
 
 
 def main():
